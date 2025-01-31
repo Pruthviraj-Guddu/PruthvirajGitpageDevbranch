49b0077fc8e8734090378973b1028e8f715b4121
Pruthviraj M A <pruthvigudduraj@gmail.com>
2025-01-30 20:12:16 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function injectData(data) {
+  let firstNameElement = document.getElementsByClassName("firstNameElement")[0];
   if (firstNameElement) {
     firstNameElement.textContent = data.basics.firstName;
   } else {
